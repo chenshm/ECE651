e686497449a205dfdf81ef238219a7e98cf9a48c
Rongzhi <gurongzhi1997@gmail.com>
2020-03-09 04:18:49 -0400

    Modify title to prj name

--- frontend/src/Capp.js
+++ frontend/src/Capp.js
@@ -8,7 +8,7 @@ import  './Capp.css';
 const  BaseLayout  = () => (
     <div  className="container-fluid">
         <nav  className="navbar navbar-expand-lg navbar-light bg-light">
-            <a  className="navbar-brand"  href="#">Django React Demo</a>
+            <a  className="navbar-brand"  href="#">Too Young Too Simple</a>
             <button  className="navbar-toggler"  type="button"  data-toggle="collapse"  data-target="#navbarNavAltMarkup"  aria-controls="navbarNavAltMarkup"  aria-expanded="false"  aria-label="Toggle navigation">
             <span  className="navbar-toggler-icon"></span>
         </button>
